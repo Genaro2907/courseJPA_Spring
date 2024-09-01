ffdebd00bf67b5c0476d2690840e45d4e70e6a24
Gabriel Genaro <g.genaro.contato@gmail.com>
2024-08-31 23:37:26 -0300

    Projected created

---
+++ src/test/java/com/treinamentoJPA/courseJPA/CourseJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinamentoJPA.courseJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
