afce7f397811bc9263483f199b9080a1e5acfa2d
Gabriel Genaro <g.genaro.contato@gmail.com>
2024-09-05 13:56:10 -0300

    Tratamento de exceção - findById

--- src/main/java/com/treinamentoJPA/courseJPA/services/UserService.java
+++ src/main/java/com/treinamentoJPA/courseJPA/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.treinamentoJPA.courseJPA.entities.User;
 import com.treinamentoJPA.courseJPA.repositories.UserRepository;
+import com.treinamentoJPA.courseJPA.services.exceptions.ResourceNotFoundException.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
